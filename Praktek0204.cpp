132ba59f3c5df1a92b11835934429e127d45c7ac
AhmadSyarwani <syarwaniahmad27@gmail.com>
2017-10-23 02:17:57 +0800

    “Menghitung dan menampilkan jumlah semua nya"

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -8,4 +8,14 @@ int main(){
 	
 	cout <<"Luas Rumah 			:"<<LuasRumah<<" m2"<<endl;
 	cout <<"Luas 1 Box Parket	:"<<Luas1Box<<" m2"<<endl;
+	
+	double jumlahBoxP,biayaP,biayaPG,totalB;
+	jumlahBoxP = LuasRumah/Luas1Box;
+	cout <<"Jumlah box paket : "<<jumlahBoxP<<endl;
+	biayaP = jumlahBoxP * 500;
+	cout <<"Biaya Parket(RP) : "<<biayaP<<endl;
+	biayaPG= LuasRumah * 20;
+	cout <<"Biaya Pasang(RP) : "<<biayaPG<<endl;
+	totalB = biayaP+biayaPG;
+	cout << "Total Biaya(RP)	: "<< totalB<<endl;
 }
