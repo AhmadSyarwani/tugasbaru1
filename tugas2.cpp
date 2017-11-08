cb9c6c2fc8835c1c79e1aa233e87cc5fa83ce47b
AhmadSyarwani <syarwaniahmad27@gmail.com>
2017-11-08 20:39:54 +0800

    ini yang benar yang tadi bidang s nya tidak bida jalan kalo pakai ini baru bisa jalan bidang s nya

--- tugas2.cpp
+++ tugas2.cpp
@@ -5,16 +5,21 @@ int main(){
 	char bidang;
 	int p,le,lu,a,t;
 	cout << "masukkan angka bidang "; cin >> bidang;
-	
-	if (bidang='p'){
-		cout << " p = "; cin >> p;
-		cout << " le = "; cin >> le;
-		lu=p*le;
-		cout << " lu p adalah " << lu;
-	}else if (bidang= 's'){
-		cout << " a = "; cin >> a;
-		cout << " t = "; cin >> t;
-		lu=0.5*a*t;
-		cout << "lu s adalah " << lu;
-	}
+		switch(bidang){
+			case 'p' :
+				cout << " p = "; cin >> p;
+				cout << " le = "; cin >> le;
+				lu=p*le;
+				cout << " lu p adalah " << lu;
+				break; // untuk mengakhiri kondisi percabangan
+			case 's' :
+				cout << " a = "; cin >> a;
+				cout << " t = "; cin >> t;
+				lu=0.5*a*t;
+				cout << " lu s adalah " << lu;
+				break;
+			default :
+				cout << " eror " << endl;
+		}
+		return 0;
 }
