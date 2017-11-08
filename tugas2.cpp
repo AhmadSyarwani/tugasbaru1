e10261320ed4075f85bd2bcc5a986f75c8cb77c0
AhmadSyarwani <syarwaniahmad27@gmail.com>
2017-11-08 20:24:48 +0800

    memasukkan rumus lu=p*le

--- tugas2.cpp
+++ tugas2.cpp
@@ -5,4 +5,11 @@ int main(){
 	char bidang;
 	int p,le,lu,a,t;
 	cout << "masukkan angka bidang "; cin >> bidang;
+	
+	if (bidang='p'){
+		cout << " p = "; cin >> p;
+		cout << " le = "; cin >> le;
+		lu=p*le;
+		cout << " lu p adalah " << lu;
+	}
 }
