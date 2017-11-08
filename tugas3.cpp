18e84d49ec4527359021f32506450ff0743c69ff
AhmadSyarwani <syarwaniahmad27@gmail.com>
2017-11-08 20:48:31 +0800

    inputan gasih menggunakan waktu lembur

--- tugas3.cpp
+++ tugas3.cpp
@@ -4,4 +4,12 @@ using namespace std;
 int main(){
 	int gp=100,jk,gt,p,l;
 	cout << " berapa jam kamu masuk kerja "; cin >> jk;
+	
+	if (jk>40){
+		l=jk-40;
+		gt=(l*1.5*gp)+(gp*40);
+		p=gt*0.15;
+		gt=gt-p;
+		cout << " ini adalah gajih kamu " << gt;
+	}
 }
