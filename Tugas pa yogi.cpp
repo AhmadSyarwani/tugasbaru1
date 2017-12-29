fc5f802f4d165dac3cb2cea22ee0698a89b4312e
AhmadSyarwani <syarwaniahmad27@gmail.com>
2017-12-29 22:16:39 +0800

    menampilkan data X

--- Tugas pa yogi.cpp
+++ Tugas pa yogi.cpp
@@ -7,5 +7,16 @@ int main(){
 	cout.setf(ios::fixed); cout.setf(ios::showpoint); cout.precision(4);
 	
 	double a1c[5]={4,4,5,3,3}; double a2c[5]={3,3,4,2,3}; double a3c[5]={5,4,2,2,2};
+	
+		cout << "MATRIKS X : "<<endl;
+	for(int i=0;i<5;i++){
+		cout << a1c[i]<<" | ";
+	}cout<<endl;
+	for(int i=0;i<5;i++){
+		cout << a2c[i]<<" | ";
+	}cout<<endl;
+	for(int i=0;i<5;i++){
+		cout << a3c[i]<<" | ";
+	}
 }
 
