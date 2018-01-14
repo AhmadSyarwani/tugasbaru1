9e2292908b0f13931e757a554627a625d091e820
AhmadSyarwani <syarwaniahmad27@gmail.com>
2018-01-14 21:20:39 +0800

    langsung saya selesai kan dan banyak tambahan nya makasih TQ

--- kuda ku lari gagah berani.cpp
+++ kuda ku lari gagah berani.cpp
@@ -10,10 +10,30 @@ class kuda{
 };
 
 int main (){
-	kuda kudaponi,zebra,kudalaut,keledai;
+	kuda kudaponi,zebra,kudalaut,keledai,kudanil;
 	
-	kudaponi.keturunan = "kecil";
-	zebra.keturunan = "warna kulit nya bergaris hitam dan putih";
-	kudalaut.keturunan = "hidup di laut";
-	keledai.keturunan = "telinga nya besar dan jalan nya lambat";
+	kudaponi.keturunan = "Badan Nya Kecil= ";
+	zebra.keturunan = "Warna Kulit Nya Bergaris Hitam Dan Putih= ";
+	kudalaut.keturunan = "Hidup Di Laut= ";
+	keledai.keturunan = "Telinga Nya Besar Dan Jalan Nya Lambat= ";
+	kudanil.keturunan = "Badan Nya Besar Gigi Nya Besar= ";
+	
+	cout << (kudaponi.keturunan);
+	kudaponi.bersuara("ngihaaaangeee");
+	
+	cout << (zebra.keturunan);
+	zebra.bersuara("hingakhingakhingak");
+	
+	cout << (kudalaut.keturunan);
+	kudalaut.bersuara("Nihil Tidak Bisa DiDengar SuaraNya Karena Dia Ada Di Dalam Air");
+	
+	cout << (keledai.keturunan);
+	keledai.bersuara("ngihok");
+	
+	cout << (kudanil.keturunan);
+	kudanil.bersuara("whoooooooo");
+}
+
+void kuda::bersuara(string bunyi){
+	cout << "suara nya "<<bunyi<<" !!"<<endl;
 }
